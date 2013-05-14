6259cd7b32f525a08647e3bbeba76235c7ca6f65
Tim Fielding <gnidleif@gmail.com>
2013-05-14 12:02:53 +0200

    Ordnade .mtl-filer och filter för plattformarna
    Minnesläckor till alla!

--- Source/GameProject/GenericHandler.cpp
+++ Source/GameProject/GenericHandler.cpp
@@ -28,9 +28,9 @@ void GenericHandler::Initialize(ID3D11Device* device, TextureManager* mTextureMg
 	mGModels["Switch"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Switch.obj", L"Data\\Models\\Collada\\");
 
 	mGModels["Platform1"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform1\\Platform1.obj", L"Data\\Models\\Collada\\Platform1\\");
-	//mGModels["Platform2"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform2.obj", L"Data\\Models\\Collada\\");
-	//mGModels["Platform3"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform3.obj", L"Data\\Models\\Collada\\");
-	//mGModels["Platform4"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform4.obj", L"Data\\Models\\Collada\\");
+	mGModels["Platform2"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform2\\Platform2.obj", L"Data\\Models\\Collada\\Platform2\\");
+	mGModels["Platform3"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform3\\Platform3.obj", L"Data\\Models\\Collada\\Platform3\\");
+	mGModels["Platform4"] = new GenericModel(device, mTextureMgr, "Data\\Models\\Collada\\Platform4\\Platform4.obj", L"Data\\Models\\Collada\\Platform4\\");
 
 	// Initializing GenericSkinnedModel objects
 	mGSModels["SkinnedModel"] = new GenericSkinnedModel(device, *mTextureMgr, "Data\\Models\\Collada\\AnimTest\\test_Collada_DAE.DAE", L"Data\\Models\\Collada\\AnimTest\\");
